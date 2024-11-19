0f7f405b5d0223632175229519f675d2c20c1468
LukasPetricek1 <lukas.petricek.business@gmail.com>
2024-11-19 17:26:31 +0100

    Init app strucutre + .css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,40 @@
 import React from "react"
+import { createBrowserRouter , RouterProvider } from "react-router-dom"
+import Root from "./pages/Root.tsx"
+
+import Signup from "./pages/SignUp.tsx"
+import HomePage from "./pages/HomePage.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import Login from "./pages/Login.tsx";
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    errorElement : <ErrorPage />,
+    element : <Root />,
+    children : [
+      {
+        index : true, 
+        element : <HomePage />
+      },
+      {
+        path : "/signup",
+        element : <Signup />
+      },
+      {
+        path: "/login",
+        element : <Login />
+      }
+    ]
+  }
+])
 
 const App : React.FC = () => { 
     return (
         <>
-            <h1>App</h1>
+            <RouterProvider router={router}>
+
+            </RouterProvider>
         </>
     )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import React from "react";
 
+import "./index.css"
+
 const root: HTMLElement = document.getElementById("root")!;
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
