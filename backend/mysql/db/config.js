1f72a0058893b4bceca439073f2130d7b42f7804
LukasPetricek1 <lukas.petricek@GYM-TISNOV.CZ>
2025-02-05 09:06:15 +0100

    zakladaci script - update

--- backend/mysql/db/config.js
+++ backend/mysql/db/config.js
@@ -2,7 +2,7 @@ const configOptions = {
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE
+    database : process.env.DB_NAME
 }
 
 module.exports = configOptions
